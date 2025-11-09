00febefc3a17bf563357b736da849d339ff954f1
Alexsander Wilen <alexsanderwilen@sancode.com.br>
2025-11-09 10:01:45 -0300

    Refatorado Usurio para ter permissao de inserção de novo usuario

--- src/app/modules/usuario/cadastro/usuario.service.ts
+++ src/app/modules/usuario/cadastro/usuario.service.ts
@@ -1,7 +1,6 @@
 ﻿import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Usuario } from './usuario.model';
-import { environment } from '../../../../environments/environment';
 import { ApiService } from '../../../core/services/api.service';
 import { Page } from '../../../shared/models/page.model';
 
@@ -10,13 +9,12 @@ import { Page } from '../../../shared/models/page.model';
 })
 export class UsuarioService {
 
-  private readonly endpoint = `${environment.apiUrl}/usuarios`;
+  private readonly endpoint = `/usuarios`;
 
   constructor(private api: ApiService) { }
 
   getUsuarios(page: number, size: number, sort: string, order: string): Observable<Page<Usuario>> {
-    const path = this.endpoint.replace(environment.apiUrl, '');
-    return this.api.page<Usuario>(path, { page, size, sort, order: order as 'asc' | 'desc' });
+    return this.api.page<Usuario>(this.endpoint, { page, size, sort, order: order as 'asc' | 'desc' });
   }
 
   getUsuario(id: number): Observable<Usuario> { return this.api.get<Usuario>(`${this.endpoint}/${id}`); }
